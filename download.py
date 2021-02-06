dbb1c26a2ba8611bc0a32d145599d312a98072dd
Antoine Dusséaux <antoine@doctrine.fr>
2021-02-06 10:02:22 +0000

    removed useless file

--- download.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import json
-
-projects_list_url = "https://commons.wikimedia.org/w/api.php?action=sitematrix&smtype=language&format=json"
-projects = json.load(projects_list_url)
-
-print(projects)
-
-# https://wikimedia.org/api/rest_v1/metrics/pageviews/top-by-country/fr.wikipedia/all-access/2021/01
